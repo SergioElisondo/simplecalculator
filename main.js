a81ed53a5efa5384386dc5cc1dc002324a487af9
Sergio <sergioelisondo@gmail.com>
2022-06-05 21:37:12 -0700

    added extra fucntionality

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-document.getElementById('button').addEventListener('click', getNumbers)
+document.getElementById('add').addEventListener('click', add)
+document.getElementById('sub').addEventListener('click', sub)
+document.getElementById('mult').addEventListener('click', mult)
+document.getElementById('divide').addEventListener('click', divide)
 
 
-function getNumbers(){
-let num1 = document.getElementById('number1').value
-let num2 = document.getElementById('number2').value
-let theMath = document.getElementById('calculation').innerHTML = parseInt(num1 * num2)
-console.log(theMath)
-return theMath
+function add(){
+    let num1 = document.getElementById('number1').value
+    let num2 = document.getElementById('number2').value
+    
+    document.getElementById('calculation').innerText = parseInt(Number(num1) + Number(num2))
+}
+function sub(){
+    let num1 = document.getElementById('number1').value
+    let num2 = document.getElementById('number2').value
+    document.getElementById('calculation').innerText = parseInt(num1 - num2)
 }
 
+function mult(){
+    let num1 = document.getElementById('number1').value
+    let num2 = document.getElementById('number2').value
+    document.getElementById('calculation').innerText = parseInt(num1 * num2)
+}
+
+function divide(){
+    let num1 = document.getElementById('number1').value
+    let num2 = document.getElementById('number2').value
+    document.getElementById('calculation').innerText = parseInt(num1 / num2)
+}
